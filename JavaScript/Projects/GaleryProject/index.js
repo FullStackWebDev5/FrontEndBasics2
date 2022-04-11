62aa1712517a92d263114e294d742c7f4d9abe53
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-04-11 21:38:43 +0530

    Gallery Project

---
+++ JavaScript/Projects/GaleryProject/index.js
@@ -0,0 +1,7 @@
+var panel1 = document.getElementById("panel-1")
+var panel2 = document.getElementById("panel-2")
+var panel3 = document.getElementById("panel-3")
+var panel4 = document.getElementById("panel-4")
+var panel5 = document.getElementById("panel-5")
+
+console.log("HI")
