b0a97d3ca8fbdadfa6bc908b5574ec9b68af90b1
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-04-15 23:48:49 +0530

    Calculator Project: Digits, . covered. Operators Started

--- JavaScript/Projects/Calculator/index.js
+++ JavaScript/Projects/Calculator/index.js
@@ -1,6 +1,28 @@
 var displayArea = document.getElementById('display')
 
-function buttonClicked(digitOrOperator){
-  displayArea.innerText = displayArea.innerText + digitOrOperator
+function isOperator(buttonText){
+  if(buttonText == '+' || buttonText == '-' || buttonText == '*' || buttonText == '/' || buttonText == '%')
+    return true
+  else 
+    return false
+}
+
+function buttonClicked(buttonText){
+  if(displayArea.innerText === '0'){
+    if(buttonText != '0' && buttonText != '00'){ // Don't do anything if button clicked is 0 or 00
+      if(buttonText == '.' || isOperator(buttonText)){ 
+        //If button clicked is . or any operators, then show it a 0 followed by button clicked (Eg: 0.5, 0+5)
+        displayArea.innerText = displayArea.innerText + buttonText
+      } else {
+        displayArea.innerText = buttonText
+      }
+    }
+  } else {
+    if(isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator((buttonText))){
+      //  replace the operator
+    } else {
+      displayArea.innerText = displayArea.innerText + buttonText
+    }
+  }
 }
 
