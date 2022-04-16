0b9e87d44f8d33f9e857d4232ff4e9a7b1474058
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-04-16 16:59:57 +0530

    Optimize Code

--- JavaScript/Projects/Calculator/index.js
+++ JavaScript/Projects/Calculator/index.js
@@ -12,42 +12,149 @@ function isOperator(buttonText) {
   else return false;
 }
 
+/* ---------------------------------------------------------------- */
+
+// function buttonClicked(buttonText) {
+//   if(displayArea.innerText.length >= 24)
+//     return
+//   if (buttonText != "CLR" && buttonText != "DEL" && buttonText != "=") {
+//     if (displayArea.innerText === "0") {
+//       if (buttonText != "0" && buttonText != "00") {
+//         // Don't do anything if button clicked is 0 or 00
+//         if (buttonText == "." || isOperator(buttonText)) {
+//           //If button clicked is . or any operators, then show it a 0 followed by button clicked (Eg: 0.5, 0+5)
+//           displayArea.innerText = displayArea.innerText + buttonText;
+//         } else {
+//           displayArea.innerText = buttonText;
+//         }
+//       }
+//     } else {
+//       if (
+//         isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
+//         isOperator(buttonText)
+//       ) {
+//         displayArea.innerText =
+//           displayArea.innerText.slice(0, -1) + buttonText;
+//       } else {
+//         displayArea.innerText = displayArea.innerText + buttonText;
+//       }
+//     }
+//   } else {
+//     if (buttonText === "CLR") {
+//       displayArea.innerText = "0";
+//     } else if (buttonText === "DEL") {
+//       displayArea.innerText = displayArea.innerText.slice(0, -1);
+//     } else {
+//       try {
+//         displayArea.innerText = eval(displayArea.innerText);
+//       } catch (error) {
+//         displayArea.innerText = 0;
+//       }
+//     }
+//   }
+// }
+
+/* ---------------------------------------------------------------- */
+
 function buttonClicked(buttonText) {
-  if (buttonText != "CLR" && buttonText != "DEL" && buttonText != "=") {
-    if (displayArea.innerText === "0") {
-      if (buttonText != "0" && buttonText != "00") {
-        // Don't do anything if button clicked is 0 or 00
-        if (buttonText == "." || isOperator(buttonText)) {
-          //If button clicked is . or any operators, then show it a 0 followed by button clicked (Eg: 0.5, 0+5)
-          displayArea.innerText = displayArea.innerText + buttonText;
-        } else {
-          displayArea.innerText = buttonText;
-        }
-      }
-    } else {
-      if (
-        isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
-        isOperator(buttonText)
-      ) {
-        displayArea.innerText = displayArea.innerText.slice(0, -1) + buttonText;
-      } else {
-        displayArea.innerText = displayArea.innerText + buttonText;
-      }
+  if(displayArea.innerText.length >= 24)
+    return
+
+  if (buttonText === "CLR") {
+    displayArea.innerText = "0"
+    return
+  }
+
+  if (buttonText === "DEL") {
+    displayArea.innerText = displayArea.innerText.slice(0, -1)
+    return
+  }
+
+  if (buttonText === "="){
+    try {
+      displayArea.innerText = eval(displayArea.innerText);
+    } catch (error) {
+      displayArea.innerText = 0;
     }
+    return
+  }
+
+  if (displayArea.innerText === "0") {
+    if (buttonText == "0" || buttonText == "00")
+      return
+
+    if (buttonText == "." || isOperator(buttonText)) {
+      displayArea.innerText = '0' + buttonText;
+      return
+    }
+
+    displayArea.innerText = buttonText;
   } else {
-    if(buttonText === 'CLR'){
-      displayArea.innerText = '0'
-    } else if (buttonText === 'DEL') {
-      displayArea.innerText = displayArea.innerText.slice(0, -1)
-    } else {
-      try {
-        displayArea.innerText = eval(displayArea.innerText)
-      } catch (error) {
-        displayArea.innerText = 0
-      }
+    if (
+      isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
+      isOperator(buttonText)
+    ) {
+      displayArea.innerText =
+        displayArea.innerText.slice(0, -1) + buttonText
+      return
     }
+
+    displayArea.innerText = displayArea.innerText + buttonText;
   }
 }
+
+/* ---------------------------------------------------------------- */
+
+// function buttonClicked(buttonText) {
+//   if (displayArea.innerText.length >= 24) return;
+
+//   if (buttonText === "CLR") {
+//     displayArea.innerText = "0";
+//     return;
+//   }
+
+//   if (buttonText === "DEL") {
+//     displayArea.innerText = displayArea.innerText.slice(0, -1);
+//     return;
+//   }
+
+//   if (buttonText === "=") {
+//     try {
+//       displayArea.innerText = eval(displayArea.innerText);
+//     } catch (error) {
+//       displayArea.innerText = 0;
+//     }
+//     return;
+//   }
+
+//   if (
+//     (buttonText == "0" || buttonText == "00") &&
+//     displayArea.innerText === "0"
+//   )
+//     return;
+
+//   if (
+//     (buttonText == "." || isOperator(buttonText)) &&
+//     displayArea.innerText === "0"
+//   ) {
+//     displayArea.innerText = "0" + buttonText;
+//     return;
+//   }
+
+//   if (
+//     isOperator(buttonText) &&
+//     isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
+//     displayArea.innerText !== "0"
+//   ) {
+//     displayArea.innerText = displayArea.innerText.slice(0, -1) + buttonText;
+//   }
+
+//   if (displayArea.innerText === "0") {
+//     displayArea.innerText = buttonText;
+//   } else {
+//     displayArea.innerText = displayArea.innerText + buttonText;
+//   }
+// }
 
 // let x = '2+3*'
 // // console.log(isOperator(x[x.length - 1]))
