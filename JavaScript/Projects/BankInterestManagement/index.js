c423a5e3c46ced6baa20527053a99c95ccf9f539
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-04-20 01:22:27 +0530

    Bank Interest Management Assignment Continue

--- JavaScript/Projects/BankInterestManagement/index.js
+++ JavaScript/Projects/BankInterestManagement/index.js
@@ -9,21 +9,36 @@
 
 /* --- Answer ---- */
 
+// let bankBalance1 = document.getElementById('bank-balance-1').value
+// console.log(bankBalance1)
+
 // let bankBalance1 = Number(prompt('Enter the first bank balance'))
 // let bankBalance2 = Number(prompt('Enter the second bank balance'))
 // let bankBalance3 = Number(prompt('Enter the third bank balance'))
 // let bankBalance4 = Number(prompt('Enter the fourth bank balance'))
 // let bankBalance5 = Number(prompt('Enter the fifth bank balance'))
 
-let bankBalances = [249389, 34561, 3462, 242442, 100001]
-//prompt("Enter all bank balances separated by a ','").split(',')
+// let bankBalances = prompt("Enter all bank balances separated by a ','").split(',')
 
-for(let i = 0; i < bankBalances.length; i++){
-  if(bankBalances[i] > 200000){
-    bankBalances[i] = Number(bankBalances[i])
-    bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.1
-    bankBalances[i] = bankBalances[i] - bankBalances[i] * 0.002
-  }
+// for(let i = 0; i < bankBalances.length; i++){
+//   bankBalances[i] = Number(bankBalances[i])
+//   if(bankBalances[i] > 200000){
+//     bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.1
+//     bankBalances[i] = bankBalances[i] - bankBalances[i] * 0.002
+//   } else if (bankBalances[i] > 100000 ) {
+//     bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.05
+//     bankBalances[i] = bankBalances[i] - bankBalances[i] * 0.0005
+//   } else {
+//     bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.03
+//   }
+// }
+
+// console.log(bankBalances)
+
+function calculateInterest () {
+  let bankBalance1 = Number(document.getElementById('bank-balance-1').value)
+  let bankBalance2 = Number(document.getElementById('bank-balance-2').value)
+  let bankBalance3 = Number(document.getElementById('bank-balance-3').value)
+
+  console.log(bankBalance1, bankBalance2, bankBalance3)
 }
-
-console.log(bankBalances)
