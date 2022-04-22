423cdc5dfc349aa6a9f84965a168c3f53dc9dc66
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-04-23 00:00:55 +0530

    Bank Interest Management Assignment Completed

--- JavaScript/Projects/BankInterestManagement/index.js
+++ JavaScript/Projects/BankInterestManagement/index.js
@@ -68,43 +68,46 @@
 
 /* ---------------------------------------------------------------- */
 
-function calculateInterest () {
-  let inputBalanceNodes = document.getElementsByClassName('input-balance')
-  
-  for(let i = 0; i < inputBalanceNodes.length; i++) {
-    console.log(inputBalanceNodes[i].value)
+function calculateInterest() {
+  let inputBalanceNodes = document.getElementsByClassName("input-balance");
+  let displayUpdatedBalances = document.getElementsByClassName("display-updated-balance");
+
+  let bankBalances = []
+
+  for (let i = 0; i < inputBalanceNodes.length; i++) {
+    bankBalances[i] = Number(inputBalanceNodes[i].value);
+    if(bankBalances[i] > 200000){
+      bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.1
+      bankBalances[i] = bankBalances[i] - bankBalances[i] * 0.002
+    } else if (bankBalances[i] > 100000 ) {
+      bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.05
+      bankBalances[i] = bankBalances[i] - bankBalances[i] * 0.0005
+    } else {
+      bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.03
+    }
+    displayUpdatedBalances[i].value = bankBalances[i]
   }
+}
 
-  let displayUpdatedBalances = document.getElementsByClassName('display-updated-balance')
+/* ---------------------------------------------------------------- */
 
-  for(let i = 0; i < displayUpdatedBalances.length; i++) {
-    displayUpdatedBalances[i].value = 2000
+function calculateInterest() {
+  let inputBalanceNodes = document.getElementsByClassName("input-balance");
+  let displayUpdatedBalances = document.getElementsByClassName("display-updated-balance");
+
+  let temp
+
+  for (let i = 0; i < inputBalanceNodes.length; i++) {
+    temp = Number(inputBalanceNodes[i].value);
+    if(temp > 200000){
+      temp = temp + temp * 0.1
+      temp = temp - temp * 0.002
+    } else if (temp > 100000 ) {
+      temp = temp + temp * 0.05
+      temp = temp - temp * 0.0005
+    } else {
+      temp = temp + temp * 0.03
+    }
+    displayUpdatedBalances[i].value = temp
   }
-
-  // let bankBalance1 = Number(document.getElementById('bank-balance-1').value)
-  // let bankBalance2 = Number(document.getElementById('bank-balance-2').value)
-  // let bankBalance3 = Number(document.getElementById('bank-balance-3').value)
-
-  // let updateBankBalance1 = document.getElementById('updated-bank-balance-1')
-  // let updateBankBalance2 = document.getElementById('updated-bank-balance-2')
-  // let updateBankBalance3 = document.getElementById('updated-bank-balance-3')
-
-  // let bankBalances = [bankBalance1, bankBalance2, bankBalance3]
-
-  // for(let i = 0; i < bankBalances.length; i++){
-  //   bankBalances[i] = Number(bankBalances[i])
-  //   if(bankBalances[i] > 200000){
-  //     bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.1
-  //     bankBalances[i] = bankBalances[i] - bankBalances[i] * 0.002
-  //   } else if (bankBalances[i] > 100000 ) {
-  //     bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.05
-  //     bankBalances[i] = bankBalances[i] - bankBalances[i] * 0.0005
-  //   } else {
-  //     bankBalances[i] = bankBalances[i] + bankBalances[i] * 0.03
-  //   }
-  // }
-
-  // updateBankBalance1.value = bankBalances[0]
-  // updateBankBalance2.value = bankBalances[1]
-  // updateBankBalance3.value = bankBalances[2]
 }
