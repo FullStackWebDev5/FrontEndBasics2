3a1dc994cc0e96640255008feeda78bed8a7e881
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-04-23 11:00:15 +0530

    Registration Form Validation Project Started

---
+++ JavaScript/Projects/RegistrationFormValidation.js/index.js
@@ -0,0 +1,10 @@
+function validate() {
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let email = document.getElementById("email").value;
+  let phoneNumber = document.getElementById("phoneNumber").value;
+  let state = document.getElementById("state").value;
+  let zipCode = document.getElementById("zipCode").value;
+  console.log("Validate works fine!!!");
+  console.log(firstName, lastName, zipCode, state, email, phoneNumber);
+}
