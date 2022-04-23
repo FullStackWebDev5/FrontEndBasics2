e0004cbaa9c898707cac120ba1724b7425b590ee
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-04-23 17:14:07 +0530

    Registration Form Validation Project Continued

--- JavaScript/Projects/RegistrationFormValidation.js/index.js
+++ JavaScript/Projects/RegistrationFormValidation.js/index.js
@@ -1,3 +1,8 @@
+let firstNameValid = document.getElementById("firstNameValid")
+let firstNameInvalid = document.getElementById("firstNameInvalid")
+let lastNameValid = document.getElementById("lastNameValid")
+let lastNameInvalid = document.getElementById("lastNameInvalid")
+
 function validate() {
   let firstName = document.getElementById("firstName").value;
   let lastName = document.getElementById("lastName").value;
@@ -5,6 +10,20 @@ function validate() {
   let phoneNumber = document.getElementById("phoneNumber").value;
   let state = document.getElementById("state").value;
   let zipCode = document.getElementById("zipCode").value;
-  console.log("Validate works fine!!!");
-  console.log(firstName, lastName, zipCode, state, email, phoneNumber);
+
+  if (firstName === ''){
+    firstNameInvalid.style.display = 'block'
+    firstNameValid.style.display = 'none'
+  } else {
+    firstNameValid.style.display = 'block'
+    firstNameInvalid.style.display = 'none'
+  }
+
+  if(lastName === ''){
+    lastNameInvalid.style.display = 'block'
+    lastNameValid.style.display = 'none'
+  } else {
+    lastNameValid.style.display = 'block'
+    lastNameInvalid.style.display = 'none'
+  }
 }
