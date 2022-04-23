b5a93cff5e8e80cf39f66e75582003f9265ebaa7
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-04-23 15:17:09 +0530

    React Intro, JSX, Create React App Folder Structure, Components, Function Components Intro, JS: Object Constructors

---
+++ React-1/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+
+
+export default App;
+
+/*
+  REACT:
+  Declarative Library
+  - Tell react WHAT to do
+  - Don't tell react HOW to do, react handles it
+
+  JSX - JavaScript + XML
+
+  Components - JavaScript Classes (Class Based Components) or Functions (Function based components) which returns JSX
+  - Parts of UI
+  - Can be nested
+  - Can be re-used
+*/
+
+
+// API - JSON (JavaScript Object Notation)
+
+/*
+  JSON
+  {
+    firstName: 'Vikrant',
+    lastName: 'Mulankar'
+  }
+
+  XML
+  <firstName>Vikrant</firstName>
+  <lastName>Mulankar</lastName>
+*/
