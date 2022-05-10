b05bec7eb5b2a51b59f0551bce193fbee3fc5e83
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-05-10 23:59:42 +0530

    JavaScript: Destructuring, Error Handling - try, catch, finally, throw keyword, Authentication Project

---
+++ JavaScript/Projects/Authentication/index.js
@@ -0,0 +1,66 @@
+let passwordStoredInDB = ''
+
+let signupInputPasswordField = document.getElementById('sign-up-input')
+let signUpValid = document.getElementById('sign-up-valid')
+let loginInputPasswordField = document.getElementById('login-input')
+let loginValid = document.getElementById('login-valid')
+let loginInvalid = document.getElementById('login-invalid')
+
+const lookup = {
+  'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q',
+  'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U',
+  'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y',
+  'M': 'Z', 'N': 'A', 'O': 'B', 'P': 'C',
+  'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G',
+  'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K',
+  'Y': 'L', 'Z': 'M',
+  'a': 'n', 'b': 'o', 'c': 'p', 'd': 'q',
+  'e': 'r', 'f': 's', 'g': 't', 'h': 'u',
+  'i': 'v', 'j': 'w', 'k': 'x', 'l': 'y',
+  'm': 'z', 'n': 'a', 'o': 'b', 'p': 'c',
+  'q': 'd', 'r': 'e', 's': 'f', 't': 'g',
+  'u': 'h', 'v': 'i', 'w': 'j', 'x': 'k',
+  'y': 'l', 'z': 'm',
+  '0': '5', '1': '6', '2': '7', '3': '8',
+  '4': '9', '5': '0', '6': '1', '7': '2',
+  '8': '3', '9': '4',
+  '!': '#', '$': '%', '&': '+', '-': '@',
+  '_': '~', '#': '!', '%': '$', '+': '&',
+  '@': '-', '~': '_'
+}
+
+const encode = (inputString) => {
+  const lookupKeys = Object.keys(lookup)
+  const lookupValues = Object.values(lookup)
+  const codeArr = inputString.split("")
+  let encodedArr = codeArr.map(codeArrChar => {
+    let index = lookupValues.findIndex((element) => element === codeArrChar)
+    return lookupKeys[index]
+  })
+  return encodedArr.join("")
+}
+
+const decode = (encodedStr) => {
+  const codeArr = encodedStr.split("")
+  let decodedArr = codeArr.map(codeArrChar => lookup[codeArrChar])
+  return decodedArr.join("")
+}
+
+const signUp = () => {
+  let inputPassword = signupInputPasswordField.value
+  passwordStoredInDB = encode(inputPassword)
+  signUpValid.style.display = 'block'
+
+  console.log(passwordStoredInDB)
+}
+
+const logIn = () => {
+  let inputPassword = loginInputPasswordField.value
+  if(inputPassword === decode(passwordStoredInDB)){
+    loginValid.style.display = 'block'
+    loginInvalid.style.display = 'none'
+  } else {
+    loginInvalid.style.display = 'block'
+    loginValid.style.display = 'none'
+  }
+}
