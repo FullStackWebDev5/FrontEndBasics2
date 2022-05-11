fbf7fa4cbcfb687ef426742a3a56e3ef9c3fa1f3
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-05-11 10:40:40 +0530

    News App: React Router setup done

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import NewsList from './components/NewsList'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/news-list' element={<NewsList />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
