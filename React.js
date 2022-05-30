b7b487d25aa56ebcd89cb8abf1db67df9774ff50
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-05-30 23:59:08 +0530

    JavaScript: Object Prototype, React - Intro, React Build and Render Illustration, Node, NPM

---
+++ React.js
@@ -0,0 +1,8 @@
+/* React
+	- All about components
+	- What to do! Not how to do!
+
+	- SPA - Single Page Application (Single html page, rest everything controlled by React.js library)
+
+	- Uses Virtual DOM
+*/
