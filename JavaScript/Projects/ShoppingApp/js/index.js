6dcf31005dc388108f4d005b1d0e378ae322ffd1
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-05-31 11:57:10 +0530

    [Building Session] Shopping App Continued - API Call onload, scroll smooth, Style Enhancements, Fixed Navbar, Spinner

--- JavaScript/Projects/ShoppingApp/js/index.js
+++ JavaScript/Projects/ShoppingApp/js/index.js
@@ -1,8 +1,10 @@
+const productsSpinner = document.getElementById('products-spinner')
+
 const fetchProducts = async () => {
 	const res = await fetch('https://fakestoreapi.com/products')
 	const data = await res.json()
-	console.log(data)
 
+	productsSpinner.style.display = 'none'
 	data.forEach(product => createProductDiv(product))
 }
 
