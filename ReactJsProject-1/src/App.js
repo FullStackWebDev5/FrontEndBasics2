188f29a5e6774eede32e62164d824b7ddb3194db
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-06-20 11:48:33 +0530

    Cart Items UI Complete

---
+++ ReactJsProject-1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react'
+import Cart from './components/Cart'
+
+export class App extends Component {
+	render() {
+		return (
+			<Cart />
+		)
+	}
+}
+
+export default App
