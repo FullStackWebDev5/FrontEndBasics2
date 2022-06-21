d00205d0fdeff8398b151dc72f3360238ca4ed6a
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-06-21 12:22:05 +0530

    [Building Session] Controlled Components in React, ToDo List App: Creare To-Do Logic complete

--- React Projects/TodoList/src/components/Todo.js
+++ React Projects/TodoList/src/components/Todo.js
@@ -8,14 +8,15 @@ import {
   Button,
   ListGroup,
 } from "react-bootstrap";
-import './Todo.css';
-import { FaEdit } from 'react-icons/fa'
-import { MdDeleteOutline, MdOutlineAddBox } from 'react-icons/md'
+import "./Todo.css";
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteOutline, MdOutlineAddBox } from "react-icons/md";
 
 export class Todo extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      inputToDo: "",
       toDoList: [
         "Revise Asynchronous Javascript",
         "Practive Array Method Questions",
@@ -26,28 +27,46 @@ export class Todo extends Component {
   }
 
   render() {
-		const { toDoList } = this.state
+    const { inputToDo, toDoList } = this.state;
     return (
       <Container style={{ margin: "20px auto" }}>
         <Col md={{ span: 4, offset: 4 }}>
           <InputGroup className="mb-3">
-            <Form.Control size="lg" type="text" placeholder="Enter To-Do" />
-            <Button variant="outline-dark" id="button-addon2">
+            <Form.Control
+              size="lg"
+              type="text"
+              placeholder="Enter To-Do"
+              value={inputToDo}
+              onChange={(e) => this.setState({ inputToDo: e.target.value })}
+            />
+            <Button
+              variant="outline-dark"
+              id="button-addon2"
+              onClick={() =>
+                this.setState((prevState) => ({
+                  toDoList: [...prevState.toDoList, inputToDo],
+                }))
+              }
+            >
               <MdOutlineAddBox />
             </Button>
           </InputGroup>
-          <ListGroup className='to-do-list'>
-						{toDoList.map(toDo => (
-							<ListGroup.Item>
-              <Row>
-                <Col md={9}>{toDo}</Col>
-                <Col md={3} className='action-btns'>
-                  <Button variant="warning" size='sm'><FaEdit /></Button>
-									<Button variant="danger" size='sm'><MdDeleteOutline /></Button>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-						))}
+          <ListGroup className="to-do-list">
+            {toDoList.map((toDo, index) => (
+              <ListGroup.Item key={index}>
+                <Row>
+                  <Col md={9}>{toDo}</Col>
+                  <Col md={3} className="action-btns">
+                    <Button variant="warning" size="sm">
+                      <FaEdit />
+                    </Button>
+                    <Button variant="danger" size="sm">
+                      <MdDeleteOutline />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
           </ListGroup>
         </Col>
       </Container>
