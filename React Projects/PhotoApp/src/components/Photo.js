b4f79ff0e099dd90bed525f78d614930b9d288d1
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-06-23 13:13:33 +0530

    React: PhotoApp: Loader added during API call, UI enhancements

--- React Projects/PhotoApp/src/components/Photo.js
+++ React Projects/PhotoApp/src/components/Photo.js
@@ -1,43 +1,52 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
-import './Photo.css'
+import { Container, Row, Col, Card, Button, Spinner } from "react-bootstrap";
+import "./Photo.css";
 
 const Photo = () => {
   const [photos, setPhotos] = useState([]);
   useEffect(() => {
     const getPhotos = async () => {
-    	try {
-    		const res = await axios.get('https://picsum.photos/v2/list')
-    		setPhotos(res.data)
-    	} catch (err) {
-    		console.log(err)
-    	}
-    }
-    getPhotos()
+      try {
+        const res = await axios.get("https://picsum.photos/v2/list");
+        setPhotos(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPhotos();
   }, []);
 
   return (
     <Container>
       <Row>
-        {photos.map((photo) => (
-          <Col key={photo.id} md={3} className='single-photo'>
-            <Card>
-              <Card.Header>{photo.author}</Card.Header>
-              <Card.Img variant="top" src={photo.download_url} />
-              <Card.Body>
-                <Button variant="primary">View Source</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+        {photos.length ? (
+          photos.map((photo) => (
+            <Col key={photo.id} md={3} className="single-photo">
+              <Card>
+                <Card.Header>{photo.author}</Card.Header>
+                <Card.Img
+                  variant="top"
+                  src={photo.download_url}
+                  className="photo-img"
+                />
+                <Card.Body>
+                  <a className="btn btn-dark" href={photo.url}>View Source</a>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))
+        ) : (
+          <div className="spinner-wrapper">
+            <Spinner animation="border" role="status"></Spinner>
+          </div>
+        )}
       </Row>
     </Container>
   );
 };
 
 export default Photo;
-
 
 // axios.get('https://picsu.photos/v2/list')
 // 	.then(res => console.log(res.data))
